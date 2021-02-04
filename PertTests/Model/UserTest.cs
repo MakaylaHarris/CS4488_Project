2e06c6efae973c6049ef4fb359a3ea1c4036cef8
Dan Walker <walkdan3@isu.edu>
2021-02-03 18:40:41 -0700

    sd

--- PertTests/Model/UserTest.cs
+++ PertTests/Model/UserTest.cs
@@ -2,27 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using WPF.Model;
+
 
 namespace PertTests.Model
 {
     [TestClass]
-    public class UserTest : User
+    public class UserTest
     {
-        public UserTest(string name, string email = "", string password = "", string username = "") : base(name, email, password, username)
-        {
-            this.Name = name;
-            this.Email = Email;
-            this.Password = Password;
-            this.Username = Username;
-        }
-        UserTest userTest1 = new UserTest("Dan");
-
-        [TestMethod]
-        public void ShowUsers()
-        {
-            userTest1.Insert();
-            
-        }
     }
 }
