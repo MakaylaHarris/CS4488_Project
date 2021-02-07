6c7e7bb93eac9e5638ffb518307000e15cbd0332
Robert Nelson <32576389+Robert-N7@users.noreply.github.com>
2021-02-07 05:43:08 -0700

    Update

--- GenerateScripts/appveyor.ps1
+++ GenerateScripts/appveyor.ps1
@@ -1,2 +1,2 @@
 
-((Get-Content -path WPF\App.config -Raw) -replace 'Data Source=\(LocalDB\)\\MSSQLLocalDB;Database=Pert;Integrated Security=True','Server=(local)\SQL2012SP1;Database=master;User ID=sa;Password=Password12!') | Set-Content -Path ..\test.txt
+((Get-Content -path WPF\App.config -Raw) -replace 'Data Source=\(LocalDB\)\\MSSQLLocalDB;Database=Pert;Integrated Security=True','Server=(local)\SQL2012SP1;Database=Pert;User ID=sa;Password=Password12!') | Set-Content -Path ..\test.txt
