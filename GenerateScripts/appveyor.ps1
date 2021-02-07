accbbb805f7c50349ba5cab44ed4f909efb8975b
Robert Nelson <32576389+Robert-N7@users.noreply.github.com>
2021-02-07 04:23:15 -0700

    Update

--- GenerateScripts/appveyor.ps1
+++ GenerateScripts/appveyor.ps1
@@ -1,4 +1,5 @@
 
-# (Get-Content -path WPF\App.config -Raw) -replace \
-# 'Data Source=(LocalDB)\MSSQLLocalDB;Database=Pert;Integrated Security=True',''
+((Get-Content -path WPF\App.config -Raw) -replace \
+'Data Source=(LocalDB)\MSSQLLocalDB;Database=Pert;Integrated Security=True','Server=(local)\SQL2016;Database=Pert;User ID=sa;Password=Password12!') \
+| Set-Content -Path WPF\App.config
 sqlcmd -S (local)\SQL2016 -U sa -P Password12! -i GenerateScripts\pertDB_create.sql
