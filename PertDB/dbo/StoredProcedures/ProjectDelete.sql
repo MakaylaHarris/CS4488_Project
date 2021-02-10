fefc083ced6dbd03439843c876dc32702fd1bd6c
Tyler Kness-Miller <tylerkm75@gmail.com>
2021-02-10 01:22:11 -0700

    Added delete sprocs for Project and Task.

---
+++ PertDB/dbo/StoredProcedures/ProjectDelete.sql
@@ -0,0 +1,15 @@
+﻿/*******************************************
+*	Procedure: Delete Project
+*	Deletes a given project based on the ID provided. 
+*	Created 2/10/2021 by Tyler Kness-Miller
+*/
+
+CREATE PROCEDURE [dbo].ProjectDelete
+(
+	@ProjectID int
+)
+AS
+BEGIN
+	DELETE FROM [dbo].Project WHERE ProjectId = @ProjectID;
+END
+GO
