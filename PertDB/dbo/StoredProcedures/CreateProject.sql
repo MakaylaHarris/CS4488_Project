2700048ce582b48e4ced4dcc6ef8af7e373d66c5
Tyler Kness-Miller <tylerkm75@gmail.com>
2021-02-09 20:17:38 -0700

    Added CreateProject sproc.

---
+++ PertDB/dbo/StoredProcedures/CreateProject.sql
@@ -0,0 +1,6 @@
+﻿/*******************************************
+*	Procedure: Create Project
+*	Attempts to create a new project.
+*	Result: True on success
+*	Created 2/8/2021 by Tyler Kness-Miller
+*/
